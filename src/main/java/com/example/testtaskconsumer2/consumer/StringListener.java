8c9e0c6d02436f22bfe6816a2c6aa25a92717f3d
Danila.Veselkov <Ote@menpM8>
2023-06-10 00:59:53 +0300

    fixes

--- src/main/java/com/example/testtaskconsumer2/consumer/StringListener.java
+++ src/main/java/com/example/testtaskconsumer2/consumer/StringListener.java
@@ -3,13 +3,10 @@ package com.example.testtaskconsumer2.consumer;
 
 import com.example.testtaskconsumer2.EnrichedStringsService;
 import com.example.testtaskconsumer2.dto.StringDto;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
-import org.springframework.validation.annotation.Validated;
 
 @Component
 @RequiredArgsConstructor
@@ -19,7 +16,7 @@ public class StringListener {
     private final EnrichedStringsService enrichedStringsService;
 
     @KafkaListener(topics = "${kafka-topic.strings-under-100}", groupId = "Some_group")
-    public void entitiesDiffListener(StringDto stringDto) {
+    public void srtringResolver(StringDto stringDto) {
         log.debug("Received string: {}", stringDto);
         var result = enrichedStringsService.saveEnrichedString(stringDto.getWord());
         log.debug("String saved: {}", result);
